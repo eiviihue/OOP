0fd45d8bafd3942f18fb60091319f96fafbd2187
Akira <155883202+eiviihue@users.noreply.github.com>
2025-10-14 15:10:28 +0700

    Update monstermart.java

--- monstermart/monstermart.java
+++ monstermart/monstermart.java
@@ -1,13 +1,6 @@
-package monstermart;
-
-public class monstermart {
+ public class monstermart {
     public static void main(String[] args) {
-        // Add some products
-        Product.addProduct(new Product("Coffee", 15000, 5));
-        Product.addProduct(new Product("Tea", 12000, 5));
-        Product.addProduct(new Product("Juice", 10000, 5));
-
-        VendingMachine machine = new VendingMachine();
-        machine.start();
+        VendingMachine vm = new VendingMachine();
+        vm.start();
     }
 }
